2e47528fcd26511e4d0f5e97531e0f88dcd2a53d
Nikolas Vago Serafini <nikolas.serafini@zrp.com.br>
2020-01-09 16:09:01 -0300

    feat(constants): added necessary aliexpress constants

---
+++ src/constants/aliexpress.js
@@ -0,0 +1,13 @@
+const aliexpressURL = "https://www.aliexpress.com/"
+const productSelector = ".search-key"
+const searchButtonSelector = ".search-button"
+const offersSelector = ".gallery-wrap"
+const offerSelector = ".list-item"
+
+module.exports = {
+  aliexpressURL,
+  productSelector,
+  searchButtonSelector,
+  offersSelector,
+  offerSelector
+}
